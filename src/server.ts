a9ae54413842edb08ce404b5c1e734e910e108bb
newecthon <ecthon@gmail.com>
2023-05-19 11:34:14 -0300

    feat: created memory CRUD

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,9 @@
 import fastify from "fastify"
-import { PrismaClient } from '@prisma/client'
+import { memoriesRoutes } from "./routes/memories"
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-// GET - listar
-// POST - criar
-// PUT - atualizar
-// PATCH - atualizar uma informação específica
-// DELETE - excluir
-
-app.get('/users', async () => {
-    const users = await prisma.user.findMany()
-    return users
-})
+app.register(memoriesRoutes)
 
 app.listen({
     port: 3333,
