f841db3d474e0a20c982307bc37591e438cc4ed3
newecthon <ecthon@gmail.com>
2023-05-19 16:43:39 -0300

    feat: created token JWT

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import 'dotenv/config'
 
 import fastify from "fastify"
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from "./routes/memories"
 import { authRoutes } from './routes/auth'
 
@@ -9,6 +10,10 @@ const app = fastify()
 
 app.register(cors, {
     origin: true // todas URLs de front-end poderão acessar nosso back-end
+})
+
+app.register(jwt, {
+    secret: 'spacetime'
 })
 
 app.register(authRoutes)
